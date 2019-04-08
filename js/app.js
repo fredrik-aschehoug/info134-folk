3bb8dcf69f596a1b2c405ba7ae61065ce3892433
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2019-04-08 18:24:21 +0200

    basic constructor done

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ var URL_wildboy_104857 = "http://wildboy.uib.no/~tpe056/folk/104857.json";
 var befolkning = new Population(URL_wildboy_104857);
 befolkning.onload = function(elements) {
     console.log(elements);
+    let names = befolkning.getNames();
+    let ids = befolkning.getIDs();
+    console.log(befolkning.getInfo(ids[1]));
    
 };
 
--- js/population.js
+++ js/population.js
@@ -7,16 +7,28 @@ function Population(url) {
 
     // Methods
     this.getNames = function () {
-
+        let names = [];
+        for (let element in this.elements) {
+            names.push(element);
+        }
+        return names;
     };
     this.getIDs = function () {
-        
+        let ids = [];
+        for (let element in this.elements) {
+            ids.push(this.elements[element].kommunenummer);
+        }
+        return ids;
     };
     this.getInfo = function (municipalID) {
-
+        let info;
+        for (let element in this.elements) {
+            if (this.elements[element].kommunenummer === municipalID) {
+                return this.elements[element];
+            }
+        }
     };
     this.load = function () {
-        const callback = this.onload;
         let xhr = new XMLHttpRequest();
         xhr.open("GET", this.url);
         xhr.onreadystatechange = (function () {
@@ -24,13 +36,11 @@ function Population(url) {
                 const response = JSON.parse(xhr.responseText);
                 this.dataset = response.datasett;
                 this.elements = response.elementer;
-                if (callback) {
-                    callback(this.elements);
+                if (this.onload) { // If the onload property has been set
+                    this.onload(this.elements);
                 }
-            
             }
         }).bind(this); // Bind the callback's 'this' to the value of Population's 'this'
         xhr.send();
-
     };
 }
