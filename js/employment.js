aeb1e65ae5d29496f288628349bd8445c30db212
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2019-04-09 11:47:30 +0200

    Inherit prototype

--- js/employment.js
+++ js/employment.js
@@ -3,44 +3,5 @@
  * @param {String} url wildboy dataset
  */
 function Employment(url) {
-    this.url = url;
-    this.onload = null;
-    this.dataset = null; // Kan strykes dersom det ikke blir brukt!
-    this.elements = null;
-
-    /**
-     * @method
-     * @param {String} municipalID
-     * @returns {Object} Employment information about the municipal
-     */
-    this.getInfo = function (municipalID) {
-        for (let element in this.elements) {
-            if (this.elements[element].kommunenummer === municipalID) {
-                let info = this.elements[element];
-                info.navn = element;
-                return info;
-            }
-        }
-    };
-    /**
-     * Uses AJAX to load JSON data from url and assign it to the object.
-     * Runs a callback when completed.
-     * @method
-     * @callback this.onload
-     */
-    this.load = function () {
-        let xhr = new XMLHttpRequest();
-        xhr.open("GET", this.url);
-        xhr.onreadystatechange = (function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                const response = JSON.parse(xhr.responseText);
-                this.dataset = response.datasett;
-                this.elements = response.elementer;
-                if (this.onload) { // If the onload property has been set
-                    this.onload();
-                }
-            }
-        }).bind(this); // Bind the callback's 'this' to the value of Population's 'this'
-        xhr.send();
-    };
+ Population.call(this, url); // Use population constructor
 }
