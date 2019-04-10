8538269a3196a1c554fdfae28a6af8a7b2c25809
IngeleivAndrea <ingeleivaum@gmail.com>
2019-04-10 09:54:04 +0200

    småfix

--- js/navigation.js
+++ js/navigation.js
@@ -1,12 +1,12 @@
 
-  var header = document.getElementById("navigasjon");
-  var btns = header.getElementsByClassName("btn");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("activmenu");
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" activmenu", "");
-    }
-    this.className += " activmenu";
-    });
+var header = document.getElementById("navigasjon");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("activmenu");
+  if (current.length > 0) {
+    current[0].className = current[0].className.replace(" activmenu", "");
   }
+  this.className += " activmenu";
+  });
+}
