fbc7df05d9b95f814afd1f51b47ff6ba613f5aed
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2019-04-29 21:29:00 +0200

    highlight tables

--- js/createComparison.js
+++ js/createComparison.js
@@ -1,9 +1,11 @@
+var test;
 /**
  * Create the entire content for the comparison view and append it to the "samanlikning" DIV
  * @param {string} id1 Municipal ID to get details from
  * @param {string} id2 Municipal ID to get details from
  */
 function createComparison(id1, id2) {
+    
     function createComparisonTable(historicalDetails, className) {
         const tables = document.createElement("div");
         const desciptions = ["Kvinner", "Menn", "Begge kjønn"];
@@ -27,6 +29,31 @@ function createComparison(id1, id2) {
         return tables;
         
     }
+    function highlightTables(table1, table2) {
+        let rows = ["popKvinner", "popMenn", "popTotal"];
+        for (let row of rows) {
+            let table1aRow1 = table1.childNodes[1].rows[row].cells;
+            let table1bRow1 = table1.childNodes[0].rows[row].cells;
+            let table2aRow1 = table2.childNodes[1].rows[row].cells;
+            let table2bRow1 = table2.childNodes[0].rows[row].cells;
+            // Start loop on third cell. First cell is heading, second does not have anything to compare to
+            for (let i = 2; i < table1aRow1.length; i++) {
+                let t1ValueNow = parseFloat(table1aRow1[i].innerText);
+                let t1ValuePast = parseFloat(table1aRow1[i - 1].innerText);
+                let t2ValueNow = parseFloat(table2aRow1[i].innerText);
+                let t2ValuePast = parseFloat(table2aRow1[i - 1].innerText);
+                let t1Diff = t1ValueNow - t1ValuePast;
+                let t2Diff = t2ValueNow - t2ValuePast;
+                if (t1Diff > t2Diff) {
+                    table1aRow1[i].classList.add("highlightedCell");
+                    table1bRow1[i].classList.add("highlightedCell");
+                } else if (t1Diff < t2Diff){
+                    table2aRow1[i].classList.add("highlightedCell");
+                    table2bRow1[i].classList.add("highlightedCell");
+                }
+            }
+        }
+    }
     const className = "employmentTable";
     // Placeholder to put content in
     const placeholder = document.getElementsByClassName("comparisonOutput");
@@ -41,6 +68,7 @@ function createComparison(id1, id2) {
     
     const table1 = createComparisonTable(historicalDetails1, className);
     const table2 = createComparisonTable(historicalDetails2, className);
+    highlightTables(table1, table2);
     // Create one tableToggle for both tables
     const tableToggle = createTableToggle(toggleCallback, className, "employment");
 
